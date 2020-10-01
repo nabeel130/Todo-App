0286d7270d7c0d6ec05881d414125c997621ec2c
Mohammed Nabeel km <nabeelkmvkd07@gmail.com>
2020-10-01 17:59:23 +0530

    Add submit and delete todo options

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .models import TodoItem
 # Create your views here.
 
@@ -9,3 +9,13 @@ def todoView(request):
         'all_items' : all_todo_items
     }
     return render(request , "todo.html" , context);
+
+def addTodo(request):
+    new_item = TodoItem(content= request.POST['content'])
+    new_item.save()
+    return HttpResponseRedirect("/todo/")
+
+def deleteTodo(request , todo_id):
+    item_to_delete = TodoItem.objects.get(id=todo_id)
+    item_to_delete.delete()
+    return HttpResponseRedirect("/todo/")
